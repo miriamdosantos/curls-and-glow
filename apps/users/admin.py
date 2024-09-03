1296595e624284e9b6622b0355168ef3f6393708
Miriam McCay <mii.santos@yahoo.com.br>
2024-09-03 15:58:50 +0100

    Add UserProfile model with signals and admin integration

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import UserProfile
 # Register your models here.
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ( 'user_display','phone_number') 
+
+    def user_display(self, obj):
+        return f"{obj.user.username} ({obj.user.email})"
+    user_display.short_description = 'User Info'
--- curlsandgrow/settings.py
+++ curlsandgrow/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ot_b=n%9vq6us^z*v+p_1zh#vm*&a)5hip#u!9o%yl53_)cr65"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -94,6 +94,11 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com",
+    "https://seusubdominio.localhost:8000",  # Exemplo de URL local
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
