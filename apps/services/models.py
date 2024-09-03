c1402121a8f71fd69777d6df951c7d582f2e20af
Miriam McCay <mii.santos@yahoo.com.br>
2024-09-03 17:13:50 +0100

    Add Service model and integrated admin configuration

--- apps/services/models.py
+++ apps/services/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Service(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    duration = models.TimeField()
+    
+
+    def __str__(self):
+        return self.title
