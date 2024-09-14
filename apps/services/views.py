2d46ea0e8327f99dee72a6a32d989ca725ed671a
Miriam McCay <mii.santos@yahoo.com.br>
2024-09-14 08:18:43 +0100

    Add Testimonials section with responsive design and styling; updated home view to render testimonials in the template.

--- apps/services/views.py
+++ apps/services/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from apps.booking.models import Offer
+from apps.booking.models import Offer, Testimonial, Booking
 from apps.services.models import Service
 from apps.stylishs.models import Stylish
 
@@ -8,10 +8,14 @@ def home (request):
     offers = Offer.objects.all()
     services = Service.objects.all().order_by('title')
     stylishs = Stylish.objects.all
+    testimonials = Testimonial.objects.all()
+    bookings = Booking.objects.all()
     context ={
         'offers':offers,
         'services':services,
-        'stylishs': stylishs
+        'stylishs': stylishs,
+        'testimonials': testimonials, 
+        'bookings': bookings
     }
     
     return render(request, 'services/index.html', context)
